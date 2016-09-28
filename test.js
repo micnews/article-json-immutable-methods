c3a60a33e023cf809b1f91652a59f080ae8bd774
ellell <lisaovermyr@gmail.com>
2016-09-28 10:11:09 +0200

    add mapText()

--- test.js
+++ test.js
@@ -1,6 +1,37 @@
 import test from 'ava';
 import 'babel-core/register';
-import {filterText} from './index';
+import {filterText, mapText} from './index';
+import {mergeDeep} from 'immutable-object-methods';
+
+test('mapText()', t => {
+  const input = [{
+    type: 'paragraph',
+    children: [
+      {content: 'foo bar', type: 'text'},
+      {type: 'embed', embedType: 'image', imageSrc: 'http://image-source.jpg'}
+  ]
+  }];
+  const actual = mapText(input, (child) => {
+    return mergeDeep(child, {bold: true});
+  });
+  const expected = [{
+    type: 'paragraph',
+    children: [
+      {content: 'foo bar', type: 'text', bold: true},
+      {type: 'embed', embedType: 'image', imageSrc: 'http://image-source.jpg'}
+    ]
+  }];
+  t.deepEqual(actual, expected);
+  t.is(actual[0].children[1], input[0].children[1]);
+});
+
+test('mapText() no change', t => {
+  const input = [{
+    type: 'paragraph',
+    children: [{content: 'foo bar', type: 'text'}]
+  }];
+  t.is(mapText(input, (child) => child), input);
+});
 
 test('filterText(), no text removed', t => {
   const input = [{
